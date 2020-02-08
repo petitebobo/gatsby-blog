655d59a3f67882b58cc8cedf9c719bef817c6474
petitebobo <claire.bk.kang@gmail.com>
2020-02-08 18:33:19 +0900

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    }
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Dev Notes by <Bobo/>`,
+    author: `PetiteBobo`,
+    description: `PetiteBobo의 개발 지식 블로그 입니다.`,
+    siteUrl: `https://petitebobo.github.io`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/petitebobo`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Written by <Styled.b>PetiteBobo</Styled.b> who lives and works in Seoul, South Korea.
+    <br />
+    Not just about programming.
+  </Fragment>
+)
